214ae41e83d05470f084ccffb2bdeeb849e15565
Fexell <91864827+AxmedAxmed@users.noreply.github.com>
2022-03-09 20:22:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,56 +30,7 @@ app.post('/f', function(req, res) {
 
 
 app.get('/', function(req, res) {
-    res.send(`
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="https://code.jquery.com/jquery-latest.js"></script>
-
-<script>
-obr()
-function isValidUrl(string) {
-    if(string.includes("file")) {
-        return true;
-    } else {
-        return false;
-    }
-};
-function obr() {
-    $.ajax({
-        type: "POST",
-        url: "/f",
-        data: {
-            a:1
-        }
-    }).done(function(r) {
-        setTimeout(obr, 1000);
-        if(r.includes(".mp4")) {
-            if($('#info').html().includes(r) || $('#info').html().includes(encodeURIComponent(btoa(r)))) return;
-            if(isValidUrl(r)) {
-                $('#info').html(\`
-                <video id="vid" width="100%" controls="none">
-                 <source src="\`+r+\`" type='video/mp4'>
-                </video>\`);
-            } else {
-                $('#info').html(\`
-                <video id="vid"  width="100%" controls="none">
-                 <source src="/static?p=\`+encodeURIComponent(btoa(r))+\`" type='video/mp4'>
-                </video>\`);
-            }
-            document.getElementById("vid").play();
-            document.getElementById("vid").addEventListener('ended', (event) => {
-                document.getElementById("vid").play();
-            })
-        } else {
-            if(isValidUrl(r)) {
-                $('#info').html("<img style=\\"max-width:100%\\" src='"+r+"'>");
-            } else {
-                $('#info').html("<img style=\\"max-width:100%\\" src='/static?p="+encodeURIComponent(btoa(r))+"'>");
-            }
-        }
-    });
-}
-</script>
-<div id="info"></div>`)
+    res.send(``)
 });
 
 
